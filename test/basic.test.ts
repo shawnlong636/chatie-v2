1aea6e55881964113746680fd3b1bba83f650cf2
Shawn Long <shawnlong636@gmail.com>
2022-12-29 19:32:22 -0800

    feat: added eslint, prettier, commitlint, stylelint

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,4 +1,4 @@
-import { describe, assert, expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { setup } from '@nuxt/test-utils'
 
 describe('My test', async () => {
