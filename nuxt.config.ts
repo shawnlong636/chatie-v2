574a98863f614ed3f50902b759497c5c405a2721
Shawn Long <shawnlong636@gmail.com>
2022-12-29 19:49:13 -0800

    feat: added global sass and added .output to .prettierignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
+import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   typescript: {
     shim: false,
   },
-
-  modules: [],
+  css: ['~/assets/scss/main.scss'],
 })
