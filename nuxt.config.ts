884df71213aa7943c374f64efdd29891a5ec6ca4
Shawn Long <shawnlong636@gmail.com>
2023-01-01 00:05:58 -0800

    fix: removed disabling user scaling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
+      viewport: 'width=device-width, initial-scale=1',
       title: 'chatie',
       meta: [
         { name: 'description', content: '' }
